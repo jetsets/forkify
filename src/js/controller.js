4a25c43997d08ac190be5e7bb07f66a2c77152ff
jetsets <maxwell.ltalexander@gmail.com>
2021-09-10 10:31:35 -0400

    Initial Commit

---
+++ src/js/controller.js
@@ -0,0 +1,125 @@
+import * as model from "./model.js";
+import { MODAL_CLOSE_SEC } from "./config.js";
+import recipeView from "./views/recipeView.js";
+import searchView from "./views/searchView.js";
+import resultsView from "./views/resultsView.js";
+import bookmarksView from "./views/bookmarksView.js";
+import paginationView from "./views/paginationView.js";
+import addRecipeView from "./views/AddRecipeView.js";
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+
+///////////////////////////////////////
+
+const controlRecipes = async function () {
+  try {
+    const id = window.location.hash.slice(1);
+
+    if (!id) return;
+    recipeView.renderSpinner();
+
+    //Loading Recipe
+    await model.loadRecipe(id);
+
+    resultsView.update(model.getSearchResultsPage());
+    bookmarksView.update(model.state.bookmarks);
+
+    //Rendering recipe
+    recipeView.render(model.state.recipe);
+  } catch (err) {
+    recipeView.renderError();
+    console.error(err);
+  }
+};
+
+const controlSearchResults = async function () {
+  try {
+    resultsView.renderSpinner();
+    //Get Search Query
+    const query = searchView.getQuery();
+    if (!query) return;
+
+    //Load Search
+    await model.loadSearchResults(query);
+    //Render Rusults
+    resultsView.render(model.getSearchResultsPage());
+
+    // Render initial pagination buttons
+    paginationView.render(model.state.search);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const controlPagination = function (goToPage) {
+  resultsView.render(model.getSearchResultsPage(goToPage));
+  paginationView.render(model.state.search);
+};
+
+const controlServings = function (newServings) {
+  //update recipe servings (in state)
+  model.updateServings(newServings);
+
+  //update reciepe view
+  recipeView.update(model.state.recipe);
+};
+
+const controlAddBookmark = function () {
+  //Add/Remove Bookmarks
+  if (!model.state.recipe.bookmarked) model.addBookmark(model.state.recipe);
+  else model.deleteBookmark(model.state.recipe.id);
+
+  //Update recipe view
+  recipeView.update(model.state.recipe);
+
+  //Render Bookmarks
+  bookmarksView.render(model.state.bookmarks);
+};
+const controlBookmarks = function () {
+  bookmarksView.render(model.state.bookmarks);
+};
+
+const controlAddRecipe = async function (newRecipe) {
+  try {
+    //render spinner
+    addRecipeView.renderSpinner();
+    //Upload the new recipe
+    await model.uploadRecipe(newRecipe);
+
+    //Render recipe
+    recipeView.render(model.state.recipe);
+
+    //success message
+    addRecipeView.renderMessage();
+
+    //render bookmark view
+    bookmarksView.render(model.state.bookmarks);
+
+    //change id in url
+    window.history.pushState(null, ``, `#${model.state.recipe.id}`);
+
+    //close form window
+    setTimeout(function () {
+      addRecipeView.toggleWindow();
+    }, MODAL_CLOSE_SEC * 1000);
+  } catch (err) {
+    console.error(`🤔🤔`, err);
+    addRecipeView.renderError(err.message);
+  }
+};
+
+const init = function () {
+  bookmarksView.addHandlerRener(controlBookmarks);
+  recipeView.addHandlerRender(controlRecipes);
+  recipeView.addHandlerUpdateServings(controlServings);
+  recipeView.addHandlerAddBookmark(controlAddBookmark);
+  searchView.addHandlerSearch(controlSearchResults);
+  paginationView.addHandlerClick(controlPagination);
+  addRecipeView.addHandlerUpload(controlAddRecipe);
+};
+
+init();
+
+const clearBookmarks = function () {
+  localStorage.clear(`bookmarks`);
+};
---
+++ src/js/views/paginationView.js
@@ -0,0 +1,63 @@
+import View from "./View.js";
+import icons from "url:../../img/icons.svg";
+
+class PaginationView extends View {
+  _parentElement = document.querySelector(`.pagination`);
+
+  addHandlerClick(handler) {
+    this._parentElement.addEventListener(`click`, function (e) {
+      const btn = e.target.closest(`.btn--inline`);
+      if (!btn) return;
+
+      const goToPage = +btn.dataset.goto;
+      handler(goToPage);
+    });
+  }
+  _generateMarkup() {
+    const currentPage = this._data.page;
+    const numPages = Math.ceil(
+      this._data.results.length / this._data.resultsPerPage
+    );
+    // page 1, and there are other pages
+    if (currentPage === 1 && numPages > 1) {
+      return this._nextPage();
+    }
+    // last page
+    if (currentPage === numPages && numPages > 1) {
+      return this._prevPage();
+    }
+    // middle page
+    if (currentPage < numPages) {
+      return this._prevPage() + this._nextPage();
+    }
+
+    // page 1 no other pages
+    return ``;
+  }
+  _prevPage() {
+    const currentPage = this._data.page;
+    return `
+            <button data-goto ="${
+              currentPage - 1
+            }" class="btn--inline pagination__btn--prev">
+                <svg class="search__icon">
+                <use href="${icons}#icon-arrow-left"></use>
+                </svg>
+                <span>Page ${currentPage - 1}</span>
+            </button>`;
+  }
+  _nextPage() {
+    const currentPage = this._data.page;
+    return `
+            <button data-goto ="${
+              currentPage + 1
+            }" class="btn--inline pagination__btn--next">
+                <span>Page ${currentPage + 1}</span>
+                <svg class="search__icon">
+                <use href="${icons}#icon-arrow-right"></use>
+                </svg>
+            </button>`;
+  }
+}
+
+export default new PaginationView();
